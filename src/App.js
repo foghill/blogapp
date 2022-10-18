832b5cbd254d5fca2ceb2621154dfcfefe3ea302
benjamindean <ben@thefoghill.com>
2022-10-18 11:29:32 -0400

    fixed routes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,21 @@
-import Navbar from './Navbar';
-import Home from './Home';
-import Create from './Create';
-import BlogDetails from './BlogDetails';
-import NotFound from './NotFound';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'; 
+import Navbar from "./Navbar";
+import Home from "./Home";
+import Create from "./Create";
+import BlogDetails from "./BlogDetails";
+import NotFound from "./NotFound";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Navbar/>
+        <Navbar />
         <div className="content">
           <Routes>
-            <Route exact path="/">
-              <Home/>
-            </Route>
-            <Route path="/create">
-              <Create/>
-            </Route>
-            <Route path="/blogs/:id">
-              <BlogDetails/>
-            </Route>
-            <Route path="*">
-              <NotFound/>
-            </Route>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<Create />} />
+            <Route path="/blogs/:id" element={<BlogDetails />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
       </div>
