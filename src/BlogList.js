fd62e7b24d6f365098f53edac24d772e1e4669f3
benjamindean <ben@thefoghill.com>
2022-10-18 15:29:15 -0400

    bloglist cleanup

--- src/BlogList.js
+++ src/BlogList.js
@@ -2,18 +2,6 @@ import { Link } from "react-router-dom";
 
 const BlogList = ({ blogs, title }) => {
   return (
-    // <div className="ui fluid container">
-    //     <h2>{title}</h2>
-    //     {blogs.map(blog => (
-    //         <div className="item" key={blog.id}>
-    //             <Link to={`/blogs/${blog.id}`}>
-    //                 <h2 class='ui orange basic button'>{blog.title}</h2>
-    //                 <p>Written by {blog.author}</p>
-    //             </Link>
-    //         </div>
-    //     ))}
-    // </div>
-
     <div class="ui centered card">
       <div class="content">
         {blogs.map((blog) => (
