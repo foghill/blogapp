e8f4a23aa094d7d07ddbd8254783c7569b3c3e77
benjamindean <ben@thefoghill.com>
2022-10-18 14:05:59 -0400

    cleaner ui

--- src/BlogList.js
+++ src/BlogList.js
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 const BlogList = ({blogs, title}) => {
     
     return ( 
-        <div className="item">
+        <div className="ui fluid container">
             <h2>{title}</h2>
             {blogs.map(blog => (
                 <div className="item" key={blog.id}>
                     <Link to={`/blogs/${blog.id}`}>
-                        <h2>{blog.title}</h2>
+                        <h2 class='ui orange basic button'>{blog.title}</h2>
                         <p>Written by {blog.author}</p>
                     </Link>
                 </div>
