323f87959137d9cded8949c6c9b046632d19e857
benjamindean <ben@thefoghill.com>
2022-10-18 11:14:56 -0400

    create page

--- src/Create.js
+++ src/Create.js
@@ -21,7 +21,33 @@ const Create = () => {
     });
   };
 
-  return <div>Create</div>;
+  return (
+    <div className="create">
+      <h2>Add a New Blog</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Blog Title</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Blog Body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        />
+        <label>Blog author:</label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="Benjamin">Benjamin</option>
+          <option value="Charles">Charles</option>
+          <option value="Dean">Dean</option>
+        </select>
+        {<button>Add Blog</button>}
+      </form>
+    </div>
+  );
 };
 
 export default Create;
