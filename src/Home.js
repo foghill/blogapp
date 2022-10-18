a8ea5d7d24738d0d2a70aab38c269aef6d5f4a52
benjamindean <ben@thefoghill.com>
2022-10-18 07:57:41 -0400

    Home component

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,16 @@
-import React from 'react'
-import BlogList from './BlogList'
-import useFetch from './useFetch'
+import React from "react";
+import BlogList from "./BlogList";
+import useFetch from "./useFetch";
+
+const API = "http://localhost:8000/blogs";
 
 const Home = () => {
+  const { data: blogs } = useFetch(API);
   return (
-    <div>Home</div>
-  )
-}
+    <div className="home">
+      <BlogList blogs={blogs} title={"All Blogs"} />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
