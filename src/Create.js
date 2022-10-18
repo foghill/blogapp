126259b0928b49ad48c8b8d8679f65b1a025f387
benjamindean <ben@thefoghill.com>
2022-10-18 11:08:12 -0400

    handlesubmit post

--- src/Create.js
+++ src/Create.js
@@ -1,19 +1,27 @@
-import React from 'react'
-import {useState} from 'react';
-import {useHistory} from 'react-router-dom'
+import React from "react";
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
-const [title,setTitle] = useState('');
-const [body,setBody] = useState('');
-const [author,setAuthor] = useState('Ben');
-const history = useHistory();
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("Ben");
+  const history = useHistory();
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
 
+    fetch("http://localhost:8000/blogs", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      history.push("/");
+    });
+  };
 
+  return <div>Create</div>;
+};
 
-  return (
-    <div>Create</div>
-  )
-}
-
-export default Create
+export default Create;
