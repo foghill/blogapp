696e9e4cdb0718b64cfd54acb0e083148bab75da
benjamindean <ben@thefoghill.com>
2022-10-18 11:06:44 -0400

    create state items

--- src/Create.js
+++ src/Create.js
@@ -1,6 +1,16 @@
 import React from 'react'
+import {useState} from 'react';
+import {useHistory} from 'react-router-dom'
 
 const Create = () => {
+const [title,setTitle] = useState('');
+const [body,setBody] = useState('');
+const [author,setAuthor] = useState('Ben');
+const history = useHistory();
+
+
+
+
   return (
     <div>Create</div>
   )
