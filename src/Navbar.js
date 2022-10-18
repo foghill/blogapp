9e967dc7d2b581703d4db4b343a30b0eed4a81dc
benjamindean <ben@thefoghill.com>
2022-10-18 11:17:12 -0400

    navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className ='navbar'>
+        <h1>React Blog</h1>
+        <div className='links'>
+            <Link to="/">Home</Link>
+            <Link to="/create">Create Blog</Link>
+        </div>
+    </nav>
   )
 }
 
