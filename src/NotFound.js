b23281b6eb39394a93c58cc1b8c62580aff74beb
benjamindean <ben@thefoghill.com>
2022-10-18 11:19:26 -0400

    notfound component

--- src/NotFound.js
+++ src/NotFound.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
-  )
-}
+    <div className="not-found">
+      <h2>404 Error</h2>
+      <p>We cannot find that page!</p>
+      <Link to="/">Take me back to Home</Link>
+    </div>
+  );
+};
 
-export default NotFound
+export default NotFound;
