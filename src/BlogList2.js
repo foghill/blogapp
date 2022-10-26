324a05a1bf1fb9addfc1943eb3538d83959af56a
benjamindean <ben@thefoghill.com>
2022-10-26 14:38:26 -0400

    blog container

--- src/BlogList2.js
+++ src/BlogList2.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import BlogCard2 from './BlogCard2';
 
 const BlogList2 = ({blogs}) => {
     const blogCards = blogs.map((blog)=>{
-        return <BlogCard2 key ={blog.id} />;
+        return <BlogCard2 key ={blog.id} blog={blog} />;
     })
   return (
     <div>{blogCards}</div>
